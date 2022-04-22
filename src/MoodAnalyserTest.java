5e7982c9ff5a622ed05a1fdde373bde99736ed14
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-22 09:48:11 +0530

    Inform user if entered Invalid Mood

--- src/MoodAnalyserTest.java
+++ src/MoodAnalyserTest.java
@@ -4,23 +4,28 @@ import org.junit.jupiter.api.Test;
 class MoodAnalyserTest {
 
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
+    public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
         String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("Sad", mood);
     }
 
     @Test
-    public void givenMessage_WhenHappy_ShouldReturnHappy(){
+    public void givenMessage_WhenHappy_ShouldReturnHappy() throws MoodAnalysisException{
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
         String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("Happy",mood);
     }
-
+    
     @Test
-    public void givenNullMood_ShouldReturnWarning(){
+    public void givenNullMood_ShouldReturnWarning() throws MoodAnalysisException{
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("Happy",mood);
+        try{
+            String mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("NULL",mood);
+        }
+        catch (MoodAnalysisException e){
+            System.out.println(e);
+        }
     }
 }
